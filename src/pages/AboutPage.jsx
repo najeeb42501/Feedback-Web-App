75da514b09b5076d9aa7a622e7dcbd86f4d25eb8
najeeb42501 <98880272+najeeb42501@users.noreply.github.com>
2023-05-20 00:08:21 +0500

    Add files via upload

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,23 @@
+import {Link} from 'react-router-dom'
+import Card from '../components/shared/Card'
+
+function AboutPage () {
+  return (
+    <Card>
+        <div className="about">
+             <h1>About This Project</h1>
+             <p> This is a React app to leave feedback for a product or service</p>
+             <p> Version: 2.0</p>
+             <p>Developed by Najeebullah Khan</p>
+
+             <Link to='/'>Back to home</Link>
+             
+
+        </div>
+
+       
+    </Card>
+  )
+}
+
+export default AboutPage
